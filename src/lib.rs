4ddefb0bab35476212fb3fedc0c37ff571d19aee
Buckram123 <m.donchenko@4irelabs.com>
2021-10-21 17:23:58 +0300

    first_commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,139 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+use near_sdk::collections::LookupSet;
+use near_sdk::json_types::U128;
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{env, ext_contract, near_bindgen, AccountId, PanicOnDefault, Promise};
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Contract {
-    // SETUP CONTRACT STATE
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct NearApps {
+    any_contracts: bool,
+    any_tags: bool,
+    approved_contracts: LookupSet<AccountId>,
+}
+
+impl Default for NearApps {
+    fn default() -> Self {
+        Self {
+            any_contracts: false,
+            any_tags: false,
+            approved_contracts: LookupSet::new(b"c"),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Tags {
+    person: String,
+    company: String,
+    purpose: String,
+}
+
+impl Tags {
+    pub fn new(person: String, company: String, purpose: String) -> Self {
+        Self {
+            person,
+            company,
+            purpose,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct ContractArgs {
+    function_name: String,
+    params: String,
+}
+
+impl ContractArgs {
+    pub fn new(function_name: String, params: String) -> Self {
+        Self {
+            function_name,
+            params,
+        }
+    }
+}
+
+#[ext_contract(ext_self)]
+pub trait ExtSelf {
+    fn callback_verify_contract_name(#[callback] val: bool, tags: Tags) -> bool;
 }
 
 #[near_bindgen]
-impl Contract {
-    // ADD CONTRACT METHODS HERE
-}
+impl NearApps {
+    // #[init]
+    // pub fn init() -> Self {
+    //     Self {
+    //         any_contracts: false,
+    //         any_tags: false,
+    //         approved_contracts: LookupSet::new(b"c"),
+    //     }
+    // }
 
-/*
- * the rest of this file sets up unit tests
- * to run these, the command will be:
- * cargo test --package rust-template -- --nocapture
- * Note: 'rust-template' comes from Cargo.toml's 'name' key
- */
-
-// use the attribute below for unit tests
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use near_sdk::test_utils::{get_logs, VMContextBuilder};
-    use near_sdk::{testing_env, AccountId};
-
-    // part of writing unit tests is setting up a mock context
-    // provide a `predecessor` here, it'll modify the default context
-    fn get_context(predecessor: AccountId) -> VMContextBuilder {
-        let mut builder = VMContextBuilder::new();
-        builder.predecessor_account_id(predecessor);
-        builder
+    pub fn call(&self, tags: Tags, contract_name: AccountId, args: ContractArgs) {
+        self.verify_tags(&tags);
+        if self.verify_contract(&contract_name) {
+            Promise::new(contract_name)
+                .function_call(
+                    args.function_name,
+                    args.params.into_bytes(),
+                    0,
+                    env::prepaid_gas() / 3,
+                )
+                .then(ext_self::callback_verify_contract_name(
+                    tags,
+                    env::current_account_id(),
+                    0,
+                    env::prepaid_gas() / 3,
+                ));
+        }
     }
 
-    // TESTS HERE
+    fn verify_contract(&self, contract_name: &AccountId) -> bool {
+        match self.any_contracts {
+            true => true,
+            false => self.approved_contracts.contains(contract_name),
+        }
+    }
+
+    fn verify_tags(&self, _tags: &Tags) {
+        if self.any_tags {
+            return;
+        } else if false {
+            env::panic_str("bad tags");
+        }
+    }
+
+    #[private]
+    pub fn add_contract(&mut self, contract_name: AccountId) {
+        self.approved_contracts.insert(&contract_name);
+    }
+
+    #[private]
+    pub fn remove_contract(&mut self, contract_name: AccountId) {
+        self.approved_contracts.remove(&contract_name);
+    }
+
+    #[private]
+    pub fn any_contracts_allowed(&mut self, any: bool) {
+        self.any_contracts = any;
+    }
+
+    #[private]
+    pub fn any_tags_allowed(&mut self, any: bool) {
+        self.any_tags = any;
+    }
+
+    #[private]
+    pub fn callback_verify_contract_name(&mut self, #[callback] val: bool, tags: Tags) -> bool {
+        let tags = format!(
+            "Person: {}\nCompany: {}\nPurpose: {}",
+            tags.person, tags.company, tags.purpose
+        );
+        env::log_str(&tags);
+        val
+    }
+
 }
---
+++ tests/tests.rs
@@ -0,0 +1,66 @@
+use near_sdk::env;
+use near_sdk::json_types::U128;
+use near_sdk::AccountId;
+use near_sdk_sim::{
+    call, deploy, init_simulator, to_yocto, ContractAccount, UserAccount, DEFAULT_GAS,
+    STORAGE_AMOUNT,
+};
+
+extern crate near_apps;
+use near_apps::{ContractArgs, NearAppsContract, Tags};
+
+near_sdk_sim::lazy_static_include::lazy_static_include_bytes! {
+    CONTRACT_BYTES => "res/near_apps.wasm",
+    TEST_FILE_BYTES => "tests/status_message.wasm",
+}
+
+fn init() -> (UserAccount, ContractAccount<NearAppsContract>, UserAccount) {
+    let mut genesis = near_sdk_sim::runtime::GenesisConfig::default();
+    genesis.gas_limit = u64::MAX;
+    genesis.gas_price = 0;
+    let master_account = init_simulator(Some(genesis));
+    let contract_account = deploy! {
+        contract: NearAppsContract,
+        contract_id: "contract",
+        bytes: &CONTRACT_BYTES,
+        signer_account: master_account
+    };
+
+    let alice = master_account.create_user(
+        AccountId::new_unchecked("alice".to_string()),
+        to_yocto("10000"),
+    );
+    alice.deploy(&TEST_FILE_BYTES, "status".parse().unwrap(), to_yocto("35"));
+    (master_account, contract_account, alice)
+}
+
+#[test]
+fn simulate_successful_call() {
+    let (master_account, near_apps, _alice) = init();
+    let status_id: near_sdk::AccountId = "status".parse().unwrap();
+    let status_amt = to_yocto("35");
+    /*let res = call!(
+        master_account,
+        near_apps.deploy_status_message(status_id.clone(), status_amt.into()),
+        STORAGE_AMOUNT,
+        DEFAULT_GAS
+    );
+    let promise_outcomes = res.get_receipt_results();
+    println!("{:#?}\n{:#?}", promise_outcomes, res);
+    */
+    let message = "hello world";
+    let res = call!(
+        master_account,
+        near_apps.call(
+            Tags::new(
+                "Mike".to_string(),
+                "Near.org".to_string(),
+                "testing".to_string(),
+            ),
+            status_id.clone(),
+            ContractArgs::new("set_status".to_string(), message.to_string(),)
+        ),
+        gas = DEFAULT_GAS * 3
+    );
+    println!("COMPLEX CALL: {:#?}", res.promise_results());
+}
